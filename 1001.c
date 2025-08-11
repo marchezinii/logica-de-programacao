8e17ca08f79296050bf69cb674b75278bd87438d
marchezinii <alexandrefelipe0307@gmail.com>
2025-08-11 17:01:59 -0300

    1001.c

---
+++ 1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A = 0;
+    int B = 0;
+    int X = 0;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    X = A+B;
+    printf("X = %d\n", X);
+ 
+    return 0;
+}
